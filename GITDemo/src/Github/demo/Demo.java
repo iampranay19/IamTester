89727c4898784738fc046f641335946c119955da
Haha CORPORATION <Haha CORPORATION@DESKTOP-01TN7MN>
2024-01-11 20:54:10 +0530

    by pranay

--- GITDemo/src/Github/demo/Demo.java
+++ GITDemo/src/Github/demo/Demo.java
@@ -2,6 +2,10 @@ package Github.demo;
 
 public class Demo {
 
+	public void m1()
+	{
+		System.out.println(" method m1");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("github demo");
