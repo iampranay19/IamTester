b70894e7dfb45575316d6fd19202485054284816
iampranay19 <156329815+iampranay19@users.noreply.github.com>
2024-01-12 09:43:53 +0530

    Git
    add one method

--- GITDemo/src/Github/demo/Demo.java
+++ GITDemo/src/Github/demo/Demo.java
@@ -12,7 +12,11 @@ public class Demo {
 	{
 		System.out.println("method m2 ");
 	}
-	
+
+	public void m3()
+	{
+		System.out.println("Method m3");
+	}
 	
 	public static void main(String[] args) 
 	{
